9c93dbcf943d3700603b2f91efa2375e38c106a3
GeorgeWithers <george.withers.2005@gmail.com>
2022-09-06 15:35:15 +0100

    Added better error handling and debug

--- Program.cs
+++ Program.cs
@@ -6,18 +6,21 @@
 
 // start the program
 
+using System.ComponentModel.Design;
+
 Console.WriteLine("Starting program\nChecking files");
 FileCheck(); // Check file integrity
+newScreen(mainMenu); // Display the main menu text
+mainMenu();
 
-Console.WriteLine(File.ReadAllText("mainMenu.txt")); // Display the main menu text
 
-while (true)
-{
+static void mainMenu()
+    {
     char key = Console.ReadKey().KeyChar;
-    if (key == 'n' || key == 'N') { Console.WriteLine("n pressed"); }
-    if (key == 'a' || key == 'A') { Console.WriteLine("a pressed"); }
-    if (key == 'c' || key == 'C') { Console.WriteLine("c pressed"); }
-    if (key == 'v' || key == 'V') { Console.WriteLine("v pressed"); }
+    if (key == 'n' || key == 'N') { newScreen("n"); }
+    if (key == 'a' || key == 'A') { newScreen("a"); }
+    if (key == 'c' || key == 'C') { newScreen("c"); }
+    if (key == 'v' || key == 'V') { newScreen("v"); }
     if (key == 'm' || key == 'M') { alreadyHome(); }
     else badInput();
     
@@ -27,29 +30,68 @@ static void newScreen(string screen)
 {
     Console.Clear();;
     Console.WriteLine(File.ReadAllText(screen + ".txt"));
-    return;
+    whatNext(screen);
 }
+
+static void whatNext(string screen)
+{
+    if (screen == "n") { newOrder(); }
+    if (screen == "a") { ammendOrder(); }
+    if (screen == "c") { cancelOrder(); }
+    if (screen == "v") { viewOrder(); }
+    else { }
+}
+
+//
+//  Function checks the integrity of required files (long function - not optimised)
+//  Data file missing gets special attention becuase I feel like it
+//
 
     static void FileCheck()
 {
     bool file = true;
+    //  Check data.txt
     file = File.Exists("data.txt");
     if (file == false) {
         Console.WriteLine("ERROR-noDataTxt");
-        { }
         noData();
     }
+    
+    //  Check mainMenu.txt
     file = File.Exists("mainMenu.txt");
-    if (file == false)
-    {
-        Console.WriteLine("ERROR-noMainMenuTxt");
-        noData();
-    }
+    if (file == false) { fileMissing("mainMenu.txt"); }
+
+    //  Check n.txt (new order)
+    file = File.Exists("n");
+    if (file == false) { fileMissing("n.txt"); }
+
+    //  Check v.txt (view order)
+    file = File.Exists("v");
+    if (file == false) { fileMissing("v.txt"); }
+
+    //  Check a.txt (ammend order)
+    file = File.Exists("a");
+    if (file == false) { fileMissing("a.txt"); }
+
+    //  Check c.txt (cancel order)
+    file = File.Exists("c");
+    if (file == false) { fileMissing("c.txt"); }
+    //  If the files pass:
     if (file == true)
     {
+        Console.WriteLine("The file check completed successfuly");
         return;
     }
+    // Should never get here but just in case of solar flares:
+    else {
+        Console.WriteLine("The file check has failed for some unknown reason, If the problem persists call support\r\nERROR-fileCheckFail")
+        killProgram(true);    
+    }
 }
+
+//
+// ERROR HANDLING AND DEBUGING FUNCS
+//
 
 static void alreadyHome() // If the user is trying to access the menu from the main menu call this
 {
@@ -79,6 +121,7 @@ static void noData() // A function to handle the lack of needed data, either rep
     {
         File.Create("data.txt");
         Console.WriteLine("The missing data has been replaced. Normal operation will now resume.");
+        return;
     }
     else
     {
@@ -87,3 +130,37 @@ static void noData() // A function to handle the lack of needed data, either rep
         System.Environment.Exit(0);
     }
 }
+
+static void badParam() // Call this if a bad parameter is passed between functions
+{
+    Console.WriteLine("The prgram encountered and internal error\r\nIf this error persists please contact support\r\n\r\nERROR-badParameter\r\n\r\n");
+    killProgram(true);
+}
+
+static void fileMissing(string file)
+{
+    Console.WriteLine("During a check of required program files one or more was missing. Missing file:");
+    Console.WriteLine(file);
+    Console.WriteLine("ERROR-MissingFile")
+    killProgram(true);
+}
+
+static void killProgram(bool error) // Takes a bool to decide if it should wait for a key press before exiting
+{
+    if (error == true)
+    {
+        Console.WriteLine("Press any key to close the program\r\n");
+        Console.ReadKey();
+        System.Environment.Exit(0);
+    }
+    else if (error == false)
+    {
+        Console.ReadKey();
+        System.Environment.Exit(0);
+    }
+    else
+    {
+        Console.WriteLine("Exit cause unknown\r\nPress any key to exit");
+        System.Environment.Exit(0);
+    }
+} 
